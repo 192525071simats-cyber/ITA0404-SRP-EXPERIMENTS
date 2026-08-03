5331ddbad3c51abd9c52c37fd32f5753ec49c550
192525071simats-cyber <192525071.simats@saveetha.com>
2026-08-03 08:16:13 +0530

    Add files via upload

---
+++ 1 lab statistics.R
@@ -0,0 +1,17 @@
+# Numeric vector
+num <- c(10, 20, 30, 40)
+
+# Character vector
+char <- c("Ram", "Ravi", "Sita")
+
+# Logical vector
+logi <- c(TRUE, FALSE, TRUE)
+
+print(num)
+print(typeof(num))
+
+print(char)
+print(typeof(char))
+
+print(logi)
+print(typeof(logi))
